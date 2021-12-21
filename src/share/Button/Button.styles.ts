9cd6ba79cf6f1c13a57ecab9fccb7ccbb7c495fb
Anastasiya Erenkova <anastasiya-erenkova@mail.ru>
2021-12-21 21:06:56 +0400

    Изменен подход написания стилей с использованием темы для кнопки

--- src/share/Button/Button.styles.ts
+++ src/share/Button/Button.styles.ts
@@ -1,17 +1,19 @@
 import styled from "styled-components";
 
 export const CustomButton = styled.button`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	font-size: 14px;
-	color: white;
-	background-color: ${({ theme }) => theme.colors.primary()};
-	padding: ${({ theme }) => `${theme.spacing(1)} ${theme.spacing(2)}`};
-	border-radius: ${({ theme }) => theme.spacing()};
-	transition: background-color ease 0.25s;
+	${({ theme }) => `
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 14px;
+		color: white;
+		background-color: ${theme.colors.primary()};
+		padding: ${theme.spacing(1)} ${theme.spacing(2)};
+		border-radius: ${theme.spacing()};
+		transition: background-color ease 0.25s;
 
-	:hover {
-		background-color: ${({ theme }) => theme.colors.primary(0.9)};
-	}
+		:hover {
+			background-color: ${theme.colors.primary(0.9)};
+		}
+	`}
 `;
