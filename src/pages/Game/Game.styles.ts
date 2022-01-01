3fd614b9be8caf90bf93db2bd1e14905c85d0658
rim <rsungatovich@gmail.com>
2022-01-01 16:17:58 +0300

    added game completion markup

--- src/pages/Game/Game.styles.ts
+++ src/pages/Game/Game.styles.ts
@@ -6,4 +6,22 @@ export const GameContent = styled.div`
 	align-items: center;
 	justify-content: center;
 	flex-grow: 1;
+
+
 `;
+
+export const Wrapper = styled.div`
+	${({ theme }) => `
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+
+		> * {
+			margin-bottom: ${theme.spacing(2)};
+
+			:last-child {
+				margin-bottom: 0;
+			}
+		}
+	`}
+`
