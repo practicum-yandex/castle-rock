0479614d200bfc541e93b4c83367641fdf5fe00a
Anastasiya Erenkova <anastasiya-erenkova@mail.ru>
2022-01-11 22:25:09 +0400

    Добавлен рандомный выбор карт, добавлены очки картам

--- src/pages/Game/Game.view.tsx
+++ src/pages/Game/Game.view.tsx
@@ -4,35 +4,42 @@ import { Canvas, GameContent } from "./Game.styles";
 import Game21 from "./Game21";
 
 const GAME_SETTINGS = {
-	Width: window.innerWidth,
-	Height: window.innerHeight
+	width: window.innerWidth,
+	height: window.innerHeight
 }
 
-const runGame = (canvasEl: HTMLCanvasElement, ctx: CanvasRenderingContext2D) => {
+const createGame = (canvasEl: HTMLCanvasElement, ctx: CanvasRenderingContext2D) => {
 	const game21 = new Game21(
-		GAME_SETTINGS.Width, 
-		GAME_SETTINGS.Height, 
+		GAME_SETTINGS.width, 
+		GAME_SETTINGS.height, 
 		canvasEl, ctx
 	);
 
 	game21.start();
+
+	return game21;
 }
 
 const Game: React.FC = () => {
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 	const contextRef = useRef<CanvasRenderingContext2D>();
+	const gameRef = useRef<Game21>();
 	const [isStart, setStartStatus] = useState(true);
 
 	useEffect(() => {
 		if (canvasRef.current) {
 			const canvasEl: HTMLCanvasElement = canvasRef.current;
-			canvasEl.width = GAME_SETTINGS.Width;
-			canvasEl.height = GAME_SETTINGS.Height;
-			contextRef.current = canvasEl.getContext('2d')  as CanvasRenderingContext2D;
+			canvasEl.width = GAME_SETTINGS.width;
+			canvasEl.height = GAME_SETTINGS.height;
+			contextRef.current = canvasEl.getContext('2d') as CanvasRenderingContext2D;
 
-			runGame(canvasEl, contextRef.current);
+			gameRef.current = createGame(canvasEl, contextRef.current);
+
+			// gameRef.current.takeCard();
 		}
 	}, []);
+
+
 
 	const renderContent = () => {
 		if (isStart && false) {
--- src/pages/Game/Game21.ts
+++ src/pages/Game/Game21.ts
@@ -1,186 +1,225 @@
 import Card from "./Card";
 
 enum CardSuit {
-    Heart = 1,
-    Spades = 2,
-    Diamonds = 3,
-    Clubs = 4
+	Heart = 1,
+	Spades = 2,
+	Diamonds = 3,
+	Clubs = 4,
 }
 
 enum CardLevels {
-    Ace = 1,
-    Two = 2,
-    Three = 3,
-    Four = 4,
-    Five = 5,
-    Six = 6,
-    Seven = 7,
-    Eight = 8,
-    Nine = 9,
-    Ten = 10,
-    Jack = 11,
-    Queen = 12,
-    King = 13
+	Ace = 1,
+	Two = 2,
+	Three = 3,
+	Four = 4,
+	Five = 5,
+	Six = 6,
+	Seven = 7,
+	Eight = 8,
+	Nine = 9,
+	Ten = 10,
+	Jack = 11,
+	Queen = 12,
+	King = 13,
 }
 
+type CartScores = {
+	// todo TS
+	[key in any]: number;
+};
+
+const cardScores: CartScores = {
+	[CardLevels[1]]: 11,
+	[CardLevels[2]]: 2,
+	[CardLevels[3]]: 3,
+	[CardLevels[4]]: 4,
+	[CardLevels[5]]: 5,
+	[CardLevels[6]]: 6,
+	[CardLevels[7]]: 7,
+	[CardLevels[8]]: 8,
+	[CardLevels[9]]: 9,
+	[CardLevels[10]]: 10,
+	[CardLevels[11]]: 2,
+	[CardLevels[12]]: 3,
+	[CardLevels[13]]: 4,
+};
+
 export default class Game21 {
-    private _launched = true;
-    private _deck: Card[] = [];
-    private _hand: Card[] = [];
-    private _sprites: HTMLImageElement | undefined;
+	private _launched = true;
+	private _deck: Card[] = [];
+	private _hand: Card[] = [];
+	private _sprites: HTMLImageElement | undefined;
 
-    constructor(
-        private _width: number, 
-        private _height: number,
-        private _canvas: HTMLCanvasElement,
-        private _ctx: CanvasRenderingContext2D
-    ) {}
+	constructor(
+		private _width: number,
+		private _height: number,
+		private _canvas: HTMLCanvasElement,
+		private _ctx: CanvasRenderingContext2D
+	) {}
 
-    init(): void {
-        this.setEvenets()
-        this.setTextFont()
-    }
+	init(): void {
+		this.setEvents();
+		this.setTextFont();
+	}
 
-    setTextFont(): void {
-        this._ctx.font = '12px Arial';
-        this._ctx.fillStyle = '#000';
-    }
+	setTextFont(): void {
+		this._ctx.font = "12px Arial";
+		this._ctx.fillStyle = "#000";
+	}
 
-    setEvenets(): void {
-        this._canvas.addEventListener('mousemove', (event: MouseEvent) => {
-            const cursorX = event.offsetX;
-            const cursorY = event.offsetY;
+	setEvents(): void {
+		this._canvas.addEventListener("mousemove", (event: MouseEvent) => {
+			const cursorX = event.offsetX;
+			const cursorY = event.offsetY;
 
-            this._canvas.style.cursor = 'auto';
+			this._canvas.style.cursor = "auto";
 
-            this._hand.forEach((card, index) => {
-                const cardCoordinates = card.coordinates;
+			this._hand.forEach((card) => {
+				const cardCoordinates = card.coordinates;
 
-                if (
-                    cardCoordinates.startX < cursorX &&
-                    cardCoordinates.startY < cursorY &&
-                    cardCoordinates.endX > cursorX &&
-                    cardCoordinates.endY > cursorY
-                ) {
-                    this._canvas.style.cursor = 'pointer';
-                }
-            })
-        })
-    }
+				if (
+					cardCoordinates.startX < cursorX &&
+					cardCoordinates.startY < cursorY &&
+					cardCoordinates.endX > cursorX &&
+					cardCoordinates.endY > cursorY
+				) {
+					this._canvas.style.cursor = "pointer";
+				}
+			});
+		});
+	}
 
-    takeCards(count: number): void {
-        this._hand = this._deck
-            .splice(0 , count)
-            .map((card, index) => {
-                const cardSpace = 20;
-                const deckWidth = 13 * cardSpace;
-                const cardDisplayWidth = card.dWidth;
-                const cardDisplayHeight = card.dHeight;
-                const cardDifference = index * cardSpace;
-                const deckPositionX = this._width / 2 - deckWidth / 2;
-                const deckPositionY = this._height / 1.5;
+	takeCard(): void {
+		const randomNumber = Math.floor(Math.random() * this._deck.length);
 
-                card.coordinates = {
-                    startX: deckPositionX + cardDifference,
-                    startY: deckPositionY,
-                    endX: deckPositionX + cardDifference + cardDisplayWidth,
-                    endY: deckPositionY + cardDisplayHeight
-                }
+		const randomCard = this._deck.splice(randomNumber, 1).map((card) => {
+			const cardSpace = 20;
+			const deckWidth = 13 * cardSpace;
+			const cardDisplayWidth = card.dWidth;
+			const cardDisplayHeight = card.dHeight;
+			const cardDifference = (this._hand.length - 1) * cardSpace;
+			const deckPositionX = this._width / 2 - deckWidth / 2;
+			const deckPositionY = this._height / 1.5;
 
-                return card;
-            })
-    }
+			card.coordinates = {
+				startX: deckPositionX + cardDifference,
+				startY: deckPositionY,
+				endX: deckPositionX + cardDifference + cardDisplayWidth,
+				endY: deckPositionY + cardDisplayHeight,
+			};
 
-    preload(cb: () => void) {
-        this.preloadSprites(cb);
-    }
+			return card;
+		});
 
-    preloadSprites(onResourceLoad: () => void): void {
-        this._sprites = new Image();
-        this._sprites.src = `./images/sprites.jpg`;
-        this._sprites.onload = onResourceLoad;
-    }
+		this._hand.push(...randomCard);
+		// this.run();
+	}
 
-    update(): void {
-        //this._ball.move()
-        //this.collideBlocks()
-    }
+	preload(cb: () => void) {
+		this.preloadSprites(cb);
+	}
 
-    create(): void {
-        this.createCards()
-    }
+	preloadSprites(onResourceLoad: () => void): void {
+		this._sprites = new Image();
+		this._sprites.src = `./images/sprites.jpg`;
+		this._sprites.onload = onResourceLoad;
+	}
 
-    createCards(): void {
-        let col = 0;
-        let row = 0;
-        const width = 225;
-        const height = 315;
-        const cardDisplyWidth = 100;
-        const cardDisplyHeight = 140;
+	update(): void {
+		//this._ball.move()
+		//this.collideBlocks()
+	}
 
-        for (let count = 0; count < 52; count++) {
-            if (count % 13 === 0 && count !== 0) {
-                col = 0;
-                row += 1;
-            } else if (count % 13 !== 0) {
-                col += 1;
-            }
+	create(): void {
+		this.createCards();
+	}
 
-            this._deck.push(new Card(
-                width * col,
-                height * row,
-                width, height,
-                cardDisplyWidth,
-                cardDisplyHeight,
-                CardSuit[row + 1],
-                CardLevels[col + 1],
-            ))
-        }
-    }
+	createCards(): void {
+		let col = 0;
+		let row = 0;
+		const width = 225;
+		const height = 315;
+		const cardDisplyWidth = 100;
+		const cardDisplyHeight = 140;
 
-    render(): void {
-        this.clearCanvas();
-        this.renderHand();
-    }
+		for (let count = 0; count < 52; count++) {
+			if (count % 13 === 0 && count !== 0) {
+				col = 0;
+				row += 1;
+			} else if (count % 13 !== 0) {
+				col += 1;
+			}
 
-    renderHand() {
-        this._hand.forEach((card) => {
-            if (this._sprites) {
-                this._ctx.drawImage(
-                    this._sprites, 
-                    card.spritesX, 
-                    card.spritesY,
-                    card.sWidth, 
-                    card.sHeight,
-                    card.coordinates.startX, 
-                    card.coordinates.startY,
-                    card.dWidth,
-                    card.dHeight
-                );
-            }
-        })
-    }
+			const cardLevel = CardLevels[col + 1];
 
-    clearCanvas(): void {
-        this._ctx.clearRect(0, 0, this._width, this._height);
-    }
+			this._deck.push(
+				new Card(
+					width * col,
+					height * row,
+					width,
+					height,
+					cardDisplyWidth,
+					cardDisplyHeight,
+					cardScores[cardLevel],
+					CardSuit[row + 1],
+					cardLevel
+				)
+			);
+		}
+	}
 
-    run(): void {
-        if (this._launched) {
-            window.requestAnimationFrame(() => {
-                this.update();
-                this.render();
-                this.run();
-            })
-        }
-    }
+	render(): void {
+		this.clearCanvas();
+		this.renderHand();
+	}
 
-    start(): void {
-        this.init()
-        this.preload(() => {
-            this.create();
-            this.takeCards(6); // remove
-            this.run();
-        })
-    }
+	renderHand() {
+		this._hand.forEach((card) => {
+			if (this._sprites) {
+				this._ctx.drawImage(
+					this._sprites,
+					card.spritesX,
+					card.spritesY,
+					card.sWidth,
+					card.sHeight,
+					card.coordinates.startX,
+					card.coordinates.startY,
+					card.dWidth,
+					card.dHeight
+				);
+			}
+		});
+	}
+
+	clearCanvas(): void {
+		this._ctx.clearRect(0, 0, this._width, this._height);
+	}
+
+	run(): void {
+		if (this._launched) {
+			window.requestAnimationFrame(() => {
+				this.update();
+				this.render();
+				this.run();
+			});
+		}
+	}
+
+	start(): void {
+		this.init();
+		this.preload(() => {
+			this.create();
+			this.takeCard(); // remove
+			this.takeCard(); // remove
+			this.takeCard(); // remove
+			this.takeCard(); // remove
+			this.run();
+		});
+	}
+
+	get scoreSum() {
+		return this._hand
+			.map((card) => card.score)
+			.reduce((acc, score) => score + acc, 0);
+	}
 }
