5127e73aaa30be8e3c958115c0eafeef7af9aa12
rim <rsungatovich@gmail.com>
2022-01-17 22:11:38 +0300

    added dispatch functions

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -7,15 +7,20 @@ export interface ChangePasswordRequest {
 } 
 
 export class UserService {
-    static changePassword(data: ChangePasswordRequest, cb: (data: string) => void): void {
-        http.put<string>('/user/password', data)
-            .then((res) => cb(res.data)) 
+    static changePassword(data: ChangePasswordRequest): (d: any) => void {
+        return (dispatch: any): void => {
+            http.put<string>('/user/password', data)
+            .then((res) => dispatch(res.data)) 
             .catch((err) => console.log(err))   
+        }
     }
 
-    static changeAvatar(data: FormData, cb: (data: UserData) => void): void {
-        http.put<UserData>('/user/profile/avatar', data)
-            .then((res) => cb(res.data)) 
+    static changeAvatar(data: FormData): (d: any) => void {
+        return (dispatch: any): void => {
+            http.put<UserData>('/user/profile/avatar', data)
+            .then((res) => dispatch(res.data)) 
             .catch((err) => console.log(err))
+        }
+
     }
 }
