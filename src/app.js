3f2bc61096175c8ecba3589209fa5aa9d89ddeb2
anastasiya-erenkova <anastasiya-erenkova@mail.ru>
2022-01-18 20:40:25 +0400

    Добавление Dockerfile, развертывание на heroku

--- src/app.js
+++ src/app.js
@@ -6,8 +6,4 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "..", "dist")));
 
-// app.get("/", function (req, res) {
-// 	res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
-// });
-
 app.listen(PORT);
