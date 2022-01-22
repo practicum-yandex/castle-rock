d48e3d615e48a78fd827e5801bd131fdab68da44
rim <rsungatovich@gmail.com>
2022-01-22 16:13:14 +0300

    added base sw

---
+++ public/sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'cr-cache-v1';
+const CACHE_URLS = ['/'];
+
+this.addEventListener("install", event => {
+    console.log("install");
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+          console.log("Opened cache");
+          return cache.addAll(CACHE_URLS);
+        })
+        .catch(err => { 
+          console.log(err);
+          throw err;
+        })
+    );
+});
+
+this.addEventListener("activate", event => {
+    console.log("activate");
+});
+
+this.addEventListener('fetch', event => { 
+    event.respondWith( 
+        caches.match(event.request) 
+            .then(response => { 
+                if (response) { 
+                    return response; 
+                } 
+
+                const fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest) 
+                    .then(response => { 
+                        if(!response || response.status !== 200 || response.type !== 'basic') { 
+                            return response; 
+                        } 
+
+                        caches.open(CACHE_NAME) 
+                            .then(cache => {
+                                cache.put(event.request, response.clone()); 
+                            });
+
+                        return response; 
+                    }); 
+            })
+    ); 
+});
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./pages/App";
 import configureStore from "./store/store";
+import { startServiceWorker } from "./helpers/startServiceWorker";
+
+startServiceWorker();
 
 export const store = configureStore();
 
