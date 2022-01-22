3da19c959303bbb94f2870b4214d5b2c6e5e52a1
Anastasiya Erenkova <anastasiya-erenkova@mail.ru>
2022-01-22 16:48:53 +0400

    Возврат BrowserRouter, версии пакета

--- src/app.js
+++ src/app.js
@@ -6,4 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "..", "dist")));
 
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "..", "dist", "index.html"));
+});
+
 app.listen(PORT);
