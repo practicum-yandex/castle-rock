283af6c82c6434261852e49db41911302856dc75
Anastasiya Erenkova <anastasiya-erenkova@mail.ru>
2022-02-01 23:05:41 +0400

    Prettier

--- src/share/Button/Button.test.tsx
+++ src/share/Button/Button.test.tsx
@@ -7,18 +7,14 @@ import Button from "./";
 describe("Button", () => {
 	it("render children", () => {
 		const TEXT = "Text Button";
-		render(
-				<Button>{TEXT}</Button>
-		);
+		render(<Button>{TEXT}</Button>);
 		expect(screen.getByText(TEXT)).toBeInTheDocument();
 	});
 
 	it("check onClick", () => {
 		const handleClick = jest.fn();
-		render(
-				<Button onClick={handleClick}>Click Me</Button>
-		);
+		render(<Button onClick={handleClick}>Click Me</Button>);
 		fireEvent.click(screen.getByText(/click me/i));
-		expect(handleClick).toHaveBeenCalledTimes(1)
+		expect(handleClick).toHaveBeenCalledTimes(1);
 	});
 });
